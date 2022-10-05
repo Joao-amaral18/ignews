096ca38bdc652afc8062a82529478a88fdae76e0
Joao-amaral18 <joaovitor.lima.amaral@gmail.com>
2022-10-05 10:38:09 -0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head"
+import { Header } from "../components/header"
+import '../styles/global.scss'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+
+    </>
+  )
+}
+
+export default MyApp
